35ad13ee539e01579c6b9978f42972bd0b9f87e3
Andy Anderson <andyanderson1818@gmail.com>
2016-10-11 13:56:41 -0700

    Updated nav bar and redirected a successful user creation to the hub, rather than a specific event

--- controllers/users.js
+++ controllers/users.js
@@ -10,7 +10,7 @@ module.exports = {
   },
 
   login: passport.authenticate('local-login', {
-    successRedirect: '/meetup/specificEvent',
+    successRedirect: '/hub',
     failureRedirect: '/login'
   })
 }
